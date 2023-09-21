3fbf8bdd3632d5e3a474f2eaac8941b5267cd235
Sandro Domianidze <sdomianidze123@gmail.com>
2023-09-21 14:50:55 +0400

    feat: render loans

---
+++ src/screens/Loans/components/Loan/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import CardView from "@/components/CardView";
+import { TLoan } from "@/types/data";
+
+const Loan: React.FC<TLoan> = ({ amount, loanee }) => {
+  return (
+    <CardView style={styles.container}>
+      <View style={styles.amountContainer}>
+        <MaterialIcons name="attach-money" size={32} />
+        <Text style={styles.amount}>{amount}</Text>
+      </View>
+      <Text style={styles.loanee}>{loanee.name}</Text>
+    </CardView>
+  );
+};
+
+export default Loan;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  amountContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  amount: {
+    fontSize: 16,
+  },
+  loanee: {
+    opacity: 0.5,
+  },
+});
--- src/screens/Loans/index.tsx
+++ src/screens/Loans/index.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+import DataContext from "@/state/DataContext";
+import Loan from "./components/Loan";
 
 const LoansScreen: React.FC = () => {
-  return <View></View>;
+  const { loans } = React.useContext(DataContext);
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={loans}
+      renderItem={({ item }) => <Loan {...item} />}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    />
+  );
 };
 
 export default LoansScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  separator: {
+    height: 16,
+  },
+});
